63c97f33e40962be5ae029d728d0ddf0002e86dc
Malikabhc <malika.bouhcine@gmail.com>
2018-07-30 13:59:35 +0200

    exercice1-4

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+    <meta charset="utf-8" />
+    <title>Exercice 1/4</title>
+  </head>
+  <body>
+    <p>
+      <?php
+        function bool() {
+          return true;
+        }
+        echo bool();
+      ?>
+    </p>
+  </body>
+</html>
